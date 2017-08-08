e8cfc774f18b4060e04fd854624d7759f3b23b73
Denys <dartist21@gmail.com>
2017-08-08 16:31:43 +0300

    fix: .product -> react component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import Header from './Header';
+import Product from './Product';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <div className="container">
+          <Product />
+        </div>
 
       </div>
     );
---
+++ src/Product.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import galleryImg from './assets/img/img1.jpg';
+
+import './Product.css';
+
+class Product extends Component {
+  render() {
+    return (
+      <section className="product">
+        <h1 className="product__title">Long Cotton Gabardine Car Coat</h1>
+        <div className="row">
+          <div className="col-xs-12 col-md-7">
+            <div className="product__gallery">
+              <img src={galleryImg}
+      		         className="gallery-img"
+                   alt="Long Cotton Gabardine Car Coat" />
+              <img src={galleryImg}
+                   className="gallery-img"
+                   alt="Long Cotton Gabardine Car Coat" />
+              <img src={galleryImg}
+           		     className="gallery-img"
+                   alt="Long Cotton Gabardine Car Coat" />
+              <img src={galleryImg}
+                   className="gallery-img"
+                   alt="Long Cotton Gabardine Car Coat" />
+            </div>
+          </div>
+          <div className="col-xs-12 col-md-5">
+            <div className="product__info">
+              <div className="product__price-number">
+                <h2 className="product__price">110 000 руб</h2>
+                <p className="product__number">Item 39428531</p>
+              </div>
+              <p className="product__color">Colour: Honey</p>
+              <div className="product__color-picker-btns">
+                <button type="button" name="color-picker" className="color-picker-btn color-picker-btn--black">Choose black color</button>
+                <button type="button" name="color-picker" className="color-picker-btn color-picker-btn--fawn"> Choose fawn color</button>
+              </div>
+            </div>
+            <div className="product__properties-btns">
+              <button type="button" name="size-picker" className="btn btn--bg-black">SELECT A SIZE</button>
+              <button type="submit" name="submit" className="btn btn--bg-white">FIND IN STORE</button>
+              <button type="button" name="size-help" className="product__size-help">NEED SIZE HELP?</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Product;
