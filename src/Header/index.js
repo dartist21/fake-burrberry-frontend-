b7a688c2221e7340617dc4cc0fbcd42a10ddb7e5
Denys <dartist21@gmail.com>
2017-08-12 10:41:04 +0300

    feat: desctop header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-import logo from "../assets/img/logo.svg";
+import logo from '../assets/img/logo.svg';
 
 const Header = styled.header`
   padding: 1rem 0;
@@ -9,11 +9,20 @@ const Header = styled.header`
   @media screen and (min-width: 48rem) {
     padding: 1.5rem 0;
   }
+  @media screen and (min-width: 62rem) {
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    border-bottom: 0;
+  }
 `;
 
 const Logo = styled.img`
   display: block;
   margin: auto;
+
+  @media screen and (min-width: 62rem) {
+    height: 1rem;
+  }
 `;
 
 export default function() {
