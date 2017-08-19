693e8ae19f1311732f71c469253c1dc5e64a1dbf
Denys <dartist21@gmail.com>
2017-08-19 07:08:42 +0300

    fix: header for mobile

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 import logo from '../assets/img/logo.svg';
+import Hamburger from './Hamburger';
 
 const Header = styled.header`
-  padding: 1rem 0;
-  border-bottom: 1px solid #c6c6c6;
+  padding: 1.125rem 0;
   @media screen and (min-width: 48rem) {
     padding: 1.5rem 0;
   }
   @media screen and (min-width: 62rem) {
     padding-top: 2rem;
     padding-bottom: 2rem;
-    border-bottom: 0;
   }
 `;
 
 const Logo = styled.img`
   display: block;
   margin: auto;
+  height: .75rem;
 
   @media screen and (min-width: 62rem) {
     height: 1rem;
@@ -28,9 +28,22 @@ const Logo = styled.img`
 export default function () {
   return (
     <Header>
-      <a href="">
-        <Logo alt="logo" src={logo} />
-      </a>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="row">
+              <div className="col-xs-2 col-md-4">
+                <Hamburger />
+              </div>
+              <div className="col-xs-8 col-md-4">
+                <a href="">
+                  <Logo alt="logo" src={logo} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Header>
   );
 }
