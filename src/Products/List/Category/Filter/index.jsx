e567775a335e115c79456628f377813ccc108766
Denys <dartist21@gmail.com>
2017-08-20 08:21:32 +0300

    fix: deleted refineby

--- src/Products/List/Category/Filter/index.jsx
+++ src/Products/List/Category/Filter/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import Option from './Option';
 
-import { ToMedium } from '../../../../Common/BreakPoints';
-
 const Filter = styled.div`
   display: flex;
   padding-bottom: 1.5rem;
@@ -12,21 +10,9 @@ const Filter = styled.div`
   overflow: scroll;
 `;
 
-const Refine = styled.p`
-  margin: 2rem 2rem 0 0;
-  font-family: Raleway;
-  font-size: 0.75rem;
-  line-height: 1.42;
-  color: #171717;
-  font-weight: 400;
-  flex-shrink: 0;
-`;
 export default () =>
   (<div className="container">
     <Filter>
-      <ToMedium>
-        <Refine>Refine by</Refine>
-      </ToMedium>
       <Option value="Category" />
       <Option value="Colour" />
       <Option value="Size" />
